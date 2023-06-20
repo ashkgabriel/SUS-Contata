25c3caef5a3ef634d86a5329d38906c54e434020
Gabriel Fortes Ashikaga <ashkgabriel@outlook.com>
2023-06-19 23:47:24 -0300

    final?

--- Scripts/scriptLogin.js
+++ Scripts/scriptLogin.js
@@ -1,8 +1,3 @@
-
-document.querySelector("#conecta").addEventListener("click", () => {
-    if (document.querySelector("input[name='optLogin']:checked").value == "cidadao") {
-      window.location.href = "./agendamentos.html";
-    } else if (document.querySelector("input[name='optLogin']:checked").value == "administrativo") {
-      window.location.href = "./agendamentos-adm.html";
-    }
-  });  
+  document.querySelector("#login-logoff").addEventListener("click", () => {
+    window.location.href = "./login.html"
+  })
